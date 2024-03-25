42307a5938741596f4363f3c53fcf4dd81612b23
HeroStudiosUWU <herobakauwu@gmail.com>
2024-03-25 02:16:32 -0300

    WebPush

---
+++ src/SunriseApi/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzpx_7X8QAwlSR0KXZgOgYbJbwTQYZxRM",
+  authDomain: "sunrisestudioserver.firebaseapp.com",
+  projectId: "sunrisestudioserver",
+  storageBucket: "sunrisestudioserver.appspot.com",
+  messagingSenderId: "1028626075366",
+  appId: "1:1028626075366:web:daef188ff1e7502a57b527",
+  measurementId: "G-8PE3TZ620V",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
---
+++ src/SunriseApi/index.js
@@ -0,0 +1,68 @@
+//Variables
+var Posts = {
+  all_posts: [
+    {
+      link_name: "post-name",
+      category: "addons",
+      title: "Post incrivel meu",
+      icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      description_resume: "Simples",
+      description: "Descriçao auto mode",
+    },
+    {
+        link_name: "post-name",
+        category: "addons",
+        title: "Items Plus",
+        icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        description_resume: "Mais Items para seu jgoo",
+        description: "Descriçao auto mode",
+    },
+    {
+        link_name: "post-name",
+        category: "textures",
+        title: "Minecraft Enchache",
+        icon: "../../images/pack_icon.png",
+        description_resume: "Melhore o visual do seu jogo",
+        description: "Descriçao auto mode",
+    },
+    {
+        link_name: "post-name",
+        category: "games",
+        title: "Lost Caves",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXbyLvTsuIde58Q20O__YkT8Ry6ASkZKrfJQ&usqp=CAU",
+        description_resume: "Aventure-se nas cavernas profundas",
+        description: "Descriçao auto mode",
+    }
+  ]
+}
+//Functions
+function GeneratePosts(postId, categoryName){
+    let PostCategory = document.getElementById(postId).getElementsByClassName("list")[0]
+    firebase.firestore().collection("posts").get().then(result =>{
+        result.docs.forEach(doc=>{
+           let postData = doc.data
+           console.log(postData)
+        //    if(postData.category == categoryName){
+        //     let postButton = document.createElement("button")
+        //     let title = document.createElement("p")
+        //     let desc = document.createElement("p")
+        //     let icon = document.createElement("img")
+        //     title.className = "content_title"
+        //     desc.className = "content_desc"
+        //     icon.className = "content_icon"
+        //     postButton.className = "content_button"
+        //     icon.src = postData.icon
+        //     title.textContent = postData.title
+        //     desc.textContent = postData.description_resume
+        //     postButton.appendChild(icon)
+        //     postButton.appendChild(title)
+        //     postButton.appendChild(desc)
+        //     PostCategory.appendChild(postButton)
+        //    }
+        })
+    })
+}
+//Start
+GeneratePosts("addons-newer", "addons")
+GeneratePosts("textures-newer", "textures")
+GeneratePosts("games-newer", "games")
