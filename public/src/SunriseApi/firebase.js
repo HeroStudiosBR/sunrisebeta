fc8ac3c5c4c8ffef5df1e8be1983b72d953adcbf
HeroStudiosUWU <herobakauwu@gmail.com>
2024-03-25 03:17:49 -0300

    Update Data

--- public/src/SunriseApi/firebase.js
+++ public/src/SunriseApi/firebase.js
@@ -1,3 +1,4 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDzpx_7X8QAwlSR0KXZgOgYbJbwTQYZxRM",
   authDomain: "sunrisestudioserver.firebaseapp.com",
@@ -5,8 +6,7 @@ const firebaseConfig = {
   storageBucket: "sunrisestudioserver.appspot.com",
   messagingSenderId: "1028626075366",
   appId: "1:1028626075366:web:daef188ff1e7502a57b527",
-  measurementId: "G-8PE3TZ620V",
+  measurementId: "G-8PE3TZ620V"
 };
-
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
